7036e04af4a7801098ae88a97e3db9894f7a0a6d
Lee Jeongyeon <leeeeeyeonn@gmail.com>
2021-08-27 21:25:25 +0900

    E2E test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
 import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [MoviesModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -11,7 +11,7 @@ export class MoviesController {
     }
 
     @Get()
-    getAll(){
+    getAll() : Movie[]{
         return this.moviesService.getAll();
     }
 
