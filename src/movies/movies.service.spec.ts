eb04c7298f87f2e07a68a6e4d0fd668156652cb5
Lee Jeongyeon <leeeeeyeonn@gmail.com>
2021-08-27 20:10:28 +0900

    unit test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -14,5 +15,89 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie & id is 1', () => {
+      service.create({
+        "title": "Test Movie",
+        "director": "me"
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+    
+    it('should throw 404 error', () => {
+      try{
+        service.getOne(999);
+      } catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('deleteOne', () => {
+    it('should delete a movie', () => {
+      service.create({
+        "title": "Test Movie",
+        "director": "me"
+      });
+      const allMovies = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toBeLessThan(allMovies);
+    });
+
+    it('should throw 404 error', () => {
+      try{
+        service.deleteOne(999);
+      } catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        "title": "Test Movie",
+        "director": "me"
+      });
+      const afterCreate = service.getAll().length;
+      
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.create({
+        "title": "Test Movie",
+        "director": "me"
+      });
+      service.update(1, {
+        "title": "Updated test"
+      });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Updated test');
+    });
+
+    it('should throw 404 error', () => {
+      try{
+        service.update(999, {});
+      } catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
